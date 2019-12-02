15bd35cfa9bdf64af6823dececb0cec5008a45f2
Toni Alatalo <toni@playsign.net>
2019-12-02 19:59:31 +0200

    2-way test commands to optima solutions server, notes / wip

--- server/oriongw.py
+++ server/oriongw.py
@@ -48,6 +48,11 @@ if __name__ == '__main__':
 	print(get_optimasolutions_test())
 	print(set_optimasolutions_test('wms|automatic'))
 
+	print(set_optimasolutions_test('wms|manual'))
+	print(set_optimasolutions_test('pss|on'))
+	print(set_optimasolutions_test('pss|off'))
+    
+
 """
 curl -X 'GET' -H 'Accept: application/json' -H 'Fiware-Service: optimasolutions_t2' -H 'Fiware-ServicePath: /lamuskakuja/piha_t2' 'http://pan0108.panoulu.net:8000/orion/v2/entities/lamuskakuja-pylvas-slcc-en-t2'
 ​
@@ -79,4 +84,34 @@ curl -X 'POST' -H 'Content-Type: text/plain' -H 'Fiware-Service: optimasolutions
 curl -X 'POST' -H 'Content-Type: text/plain' -H 'Fiware-Service: optimasolutions_t2' -H 'Fiware-ServicePath: /lamuskakuja/piha_t2' \
 -d 'pss|off' \
 '{http://pan0108.panoulu.net}:8000/idasdata/iot/d?k=lamuskakuja-pylvas-slg-en-t2-apikey&i=lamuskakuja-pylvas-slg-en-dev-t2'
+"""
+
+#---
+
+"""
+# Ohjaus tapahtuu seuraavalla tavalla:
+
+# workingModeSet ==> tilaan automatic
+curl -X 'POST' -H 'Content-Type: text/plain' -H 'Fiware-Service: optimasolutions_t2' -H 'Fiware-ServicePath: /lamuskakuja/piha_t2' \
+-d 'wms|automatic' \
+'{http://pan0108.panoulu.net}:8000/idasdata/iot/d?k=lamuskakuja-pylvas-slcc-en-t2-apikey&i=lamuskakuja-pylvas-slcc-en-dev-t2'
+
+# workingModeSet ==> tilaan manual
+
+curl -X 'POST' -H 'Content-Type: text/plain' -H 'Fiware-Service: optimasolutions_t2' -H 'Fiware-ServicePath: /lamuskakuja/piha_t2' \
+-d 'wms|manual' \
+'{http://pan0108.panoulu.net}:8000/idasdata/iot/d?k=lamuskakuja-pylvas-slcc-en-t2-apikey&i=lamuskakuja-pylvas-slcc-en-dev-t2'
+
+# powerStateSet ==> tilaan on
+
+curl -X 'POST' -H 'Content-Type: text/plain' -H 'Fiware-Service: optimasolutions_t2' -H 'Fiware-ServicePath: /lamuskakuja/piha_t2' \
+-d 'pss|on' \
+'{http://pan0108.panoulu.net}:8000/idasdata/iot/d?k=lamuskakuja-pylvas-slg-en-t2-apikey&i=lamuskakuja-pylvas-slg-en-dev-t2'
+
+# powerStateSet ==> tilaan off
+
+curl -X 'POST' -H 'Content-Type: text/plain' -H 'Fiware-Service: optimasolutions_t2' -H 'Fiware-ServicePath: /lamuskakuja/piha_t2' \
+-d 'pss|off' \
+'{http://pan0108.panoulu.net}:8000/idasdata/iot/d?k=lamuskakuja-pylvas-slg-en-t2-apikey&i=lamuskakuja-pylvas-slg-en-dev-t2'
+
 """
