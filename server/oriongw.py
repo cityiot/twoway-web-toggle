9f69695af3f3850a10e0b14cc2ad5c4f884530ce
Toni Alatalo <toni@playsign.net>
2019-12-03 15:14:53 +0200

    two way backend conf: lamuskakuja -> riekonmarja

--- server/oriongw.py
+++ server/oriongw.py
@@ -26,7 +26,7 @@ def get_from_orion(entity_id="", fiware_service="example", fiware_service_path="
 	return rjson
 
 def get_optimasolutions_test1():
-	rjson1 = get_from_orion('lamuskakuja-pylvas-slcc-en-t2', 'optimasolutions_t2', '/lamuskakuja/piha_t2', 'http://pan0108.panoulu.net:8000')
+	rjson1 = get_from_orion('riekonmarja-pylvas-slcc-en-t4', 'optimasolutions_t4', '/riekonmarja/pihapylvas_t4', 'http://pan0108.panoulu.net:8000')
 
 	#wmodedata = rjson['workingMode']
 	#wmode = wmodedata['value']
@@ -35,11 +35,11 @@ def get_optimasolutions_test1():
 	return rjson1
 
 def get_optimasolutions_test2():
-    rjson2 = get_from_orion('lamuskakuja-pylvas-slg-en-t2', 'optimasolutions_t2', '/lamuskakuja/piha_t2', 'http://pan0108.panoulu.net:8000')
+    rjson2 = get_from_orion('riekonmarja-pylvas-slg-en-t4', 'optimasolutions_t4', '/riekonmarja/pihapylvas_t4', 'http://pan0108.panoulu.net:8000')
     return rjson2
 
 def set_optimasolutions_test(idasparams, idasdata):
-	headers = make_headers('optimasolutions_t2', '/lamuskakuja/piha_t2')
+	headers = make_headers('optimasolutions_t4', '/riekonmarja/pihapylvas_t4')
 	#request_url = f"{base_url}{base_path}{entity_id}"
 	request_url = f"http://pan0108.panoulu.net:8000/idasdata/iot/d?{idasparams}"
 
@@ -51,21 +51,21 @@ def set_optimasolutions_test(idasparams, idasdata):
 	return responsetext 
 
 def set_optimasolutions_test1(idasdata):
-    idasparams = "k=lamuskakuja-pylvas-slcc-en-t2-apikey&i=lamuskakuja-pylvas-slcc-en-dev-t2"
+    idasparams = "k=riekonmarja-pylvas-slcc-en-t4-apikey&i=riekonmarja-pylvas-slcc-en-dev-t4"
     return set_optimasolutions_test(idasparams, idasdata)
 
 def set_optimasolutions_test2(idasdata):
-    idasparams = "k=lamuskakuja-pylvas-slg-en-t2-apikey&i=lamuskakuja-pylvas-slg-en-dev-t2"
+    idasparams = "k=riekonmarja-pylvas-slg-en-t4-apikey&i=riekonmarja-pylvas-slg-en-dev-t4"
     return set_optimasolutions_test(idasparams, idasdata)
 
 if __name__ == '__main__':
-	print(get_optimasolutions_test())
-	print(set_optimasolutions_test('wms|automatic'))
+	print(get_optimasolutions_test1())
+	#print(set_optimasolutions_test('wms|automatic'))
 
-	print(set_optimasolutions_test('wms|manual'))
-	print(set_optimasolutions_test('pss|on'))
-	print(set_optimasolutions_test('pss|off'))
-    
+	#print(set_optimasolutions_test('wms|manual'))
+	#print(set_optimasolutions_test('pss|on'))
+	#print(set_optimasolutions_test('pss|off'))
+
 
 """
 curl -X 'GET' -H 'Accept: application/json' -H 'Fiware-Service: optimasolutions_t2' -H 'Fiware-ServicePath: /lamuskakuja/piha_t2' 'http://pan0108.panoulu.net:8000/orion/v2/entities/lamuskakuja-pylvas-slcc-en-t2'
